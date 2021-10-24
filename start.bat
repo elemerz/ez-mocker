67c0d028215112014a891a89b65c6225abf9cb8e
elemerz <elemerz@gmail.com>
2021-10-24 17:50:45 +0300

    Starting to make Mocker Maven-central-aware.

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
 color 2f
-java -agentlib:jdwp=transport=dt_socket,address=7000,server=y,suspend=n -jar .\target\ez-mocker-1.0-SNAPSHOT.jar
+java -agentlib:jdwp=transport=dt_socket,address=7000,server=y,suspend=n -jar .\target\ez-mocker-0.9.0-SNAPSHOT.jar
